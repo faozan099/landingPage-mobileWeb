a0097f30fe9b010f3c64538d953b180e0f2caebd
Your Name <fauzanaunillah13@gmail.comgit config --global user.email fauzanaunillah13@gmail.com>
2023-09-19 23:08:50 +0700

    menambahkan DOM

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // alert('Mohon maaf web kami akan lebih optimal jika dalam tampilan mobile')
+
+const imgElements = document.querySelectorAll("img[data-url]");
+
+imgElements.forEach(function (imgElement) {
+  imgElement.addEventListener("click", () => {
+    const url = imgElement.getAttribute("data-url");
+    window.location.href = url;
+  });
+  imgElement.style.cursor = "pointer";
+});
+
+const pElements = document.querySelectorAll("p[data-url]");
+
+pElements.forEach(function (pElement) {
+  pElement.addEventListener("click", () => {
+    const url = pElement.getAttribute("data-url");
+    window.location.href = url;
+  });
+  pElement.style.cursor = "pointer";
+});
